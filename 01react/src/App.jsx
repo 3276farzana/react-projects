8fc40e9942050e1c0d46c4049d7490567fcbcb01
FARZANA KAMAL BHUYIA <2014751065@uits.edu.bd>
2024-05-24 13:12:29 +0600

    interview

---
+++ 01react/src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+
+
+function App() {
+  const[counter,setCounter]=useState(12);
+
+  const addValue=()=>{
+      if(counter<20)
+        {
+        setCounter(counter+1);
+        console.log(`clicked ${setCounter}`);
+      }
+  }
+  const removeValue=()=>{
+    if(counter>0){
+    setCounter(counter-1);
+    console.log(`clicked ${setCounter}`);
+    }
+
+  }
+  return (
+    <>
+    <h1>heloo</h1>
+    <h2>Counter Value: {counter}</h2>
+    <button onClick={addValue}>Add Value</button><br />
+    <button onClick={removeValue}>Remove Value</button>
+    </>
+  )
+}
+
+export default App;
